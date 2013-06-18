357ba7ff490b34e72f1c549ddec6c69fec15f574
Mung <munglore@gmail.com>
2013-06-18 19:49:26 -0400

    Moved calculations out of AngleCriterion into JointAnalyzer

--- KinectTherapy/KinectTherapy/Criteria/JointAnalyzer.cs
+++ KinectTherapy/KinectTherapy/Criteria/JointAnalyzer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Kinect;
 using Microsoft.Xna.Framework;
+using System.Diagnostics;
 
 namespace SWENG.Criteria
 {
@@ -129,6 +130,38 @@ namespace SWENG.Criteria
         public static bool isBehind(Joint aligningJoint, Joint jointToBeAlignedTo)
         {
             return alignedHorizontally(aligningJoint, jointToBeAlignedTo) < 0;
+        }
+
+        /// <summary>
+        /// Determines the angle between three joints
+        /// </summary>
+        /// <param name="vertexJoint"></param>
+        /// <param name="adjacentJoints"></param>
+        /// <returns></returns>
+        public static int findAngle(Joint vertexJoint, Joint[] adjacentJoints)
+        {
+            // get the vertex and other joints off the skeleton stamp
+            Joint joint0 = adjacentJoints[0];
+            Joint joint1 = adjacentJoints[1];
+            // get vector vertex -> joint0
+
+            Vector3 vector0 = new Vector3(joint0.Position.X - vertexJoint.Position.X, joint0.Position.Y - vertexJoint.Position.Y, joint0.Position.Z - vertexJoint.Position.Z);
+            // get vector vertex -> joint1
+            Vector3 vector1 = new Vector3(joint1.Position.X - vertexJoint.Position.X, joint1.Position.Y - vertexJoint.Position.Y, joint1.Position.Z - vertexJoint.Position.Z);
+
+            //// determine the angle from two vectors in a 2D space...
+            // the old way
+            //float computedAngle = (float)Math.Atan2(vector0.Y, vector0.X) - (float)Math.Atan2(vector1.Y, vector1.X);
+            //// compare to the varience
+            //int convertedAngle = Convert.ToInt32(computedAngle * (180.0 / Math.PI));
+
+            // the right way we're in 3D
+            // using vector provided methods
+            vector0.Normalize();
+            vector1.Normalize();
+            float dotAngle = (float)Math.Acos(Vector3.Dot(vector0, vector1));
+            int convertedDotAngle = Convert.ToInt32(dotAngle * (180.0 / Math.PI));
+            return convertedDotAngle;
         }
     }
 }
