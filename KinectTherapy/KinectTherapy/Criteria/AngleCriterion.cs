d43622844d67388c08150f73c34f88b5971fda52
Mung <munglore@gmail.com>
2013-06-18 19:49:47 -0400

    missed a file to commit

--- KinectTherapy/KinectTherapy/Criteria/AngleCriterion.cs
+++ KinectTherapy/KinectTherapy/Criteria/AngleCriterion.cs
@@ -72,26 +72,11 @@ namespace SWENG.Criteria
         {
             // get the vertex and other joints off the skeleton stamp
             Joint vertexJoint = skeletonStamp.GetTrackedSkeleton().Joints[Vertex.GetJointType()];
-            Joint joint0 = skeletonStamp.GetTrackedSkeleton().Joints[OtherJoints[0].GetJointType()];
-            Joint joint1 = skeletonStamp.GetTrackedSkeleton().Joints[OtherJoints[1].GetJointType()];
-            // get vector vertex -> joint0
-            
-            Vector3 vector0 = new Vector3(joint0.Position.X - vertexJoint.Position.X, joint0.Position.Y - vertexJoint.Position.Y, joint0.Position.Z - vertexJoint.Position.Z);
-            // get vector vertex -> joint1
-            Vector3 vector1 = new Vector3(joint1.Position.X - vertexJoint.Position.X, joint1.Position.Y - vertexJoint.Position.Y, joint1.Position.Z - vertexJoint.Position.Z);
-
-            //// determine the angle from two vectors in a 2D space...
-           // the old way
-            //float computedAngle = (float)Math.Atan2(vector0.Y, vector0.X) - (float)Math.Atan2(vector1.Y, vector1.X);
-            //// compare to the varience
-            //int convertedAngle = Convert.ToInt32(computedAngle * (180.0 / Math.PI));
-            
-            // the right way we're in 3D
-            // using vector provided methods
-            vector0.Normalize();
-            vector1.Normalize();
-            float dotAngle = (float)Math.Acos(Vector3.Dot(vector0, vector1));
-            int convertedDotAngle = Convert.ToInt32(dotAngle * (180.0 / Math.PI));
+            Joint[] adjacentJoints = new Joint[2];
+            adjacentJoints[0] = skeletonStamp.GetTrackedSkeleton().Joints[OtherJoints[0].GetJointType()];
+            adjacentJoints[1] = skeletonStamp.GetTrackedSkeleton().Joints[OtherJoints[1].GetJointType()];
+           
+            int convertedDotAngle = JointAnalyzer.findAngle(vertexJoint,adjacentJoints);
             Debug.WriteLine("Angle: " + convertedDotAngle);
             return convertedDotAngle > MinimumAngle && convertedDotAngle < MaximumAngle;
         }
